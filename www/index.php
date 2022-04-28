2921f080413b16e8b36917efd554c7b9f4b64292
choi <kombo6768@gmail.com>
2022-04-28 11:01:26 +0900

    Docker Compose

--- www/index.php
+++ www/index.php
@@ -1,15 +1,116 @@
-<h1>Hello Cloudreach!</h1>
-<h4>Attempting MySQL connection from php...</h4>
 <?php 
-$host = 'mysql';
-$user = 'root';
-$pass = 'dkfmadl';
-$conn = new mysqli($host, $user, $pass);
+  $host = 'mysql';
+  $user = 'root';
+  $pass = 'dkfmadl';
+  $conn = new mysqli($host, $user, $pass);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected to MySQL successfully!";
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
 
-phpinfo();
-?>
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <style>
+    .test {
+      color: blue;
+    }
+    .mt-20 {
+      margin-top: 20%;
+    }
+    table,tr,td,th {
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
+    thead {
+      background-color: #888;
+    }
+    .wid100 {
+      width: 100%;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <div id="main">
+    <h2>Test Table</h2>
+
+    <label>Options</label>
+    <div>
+      <label>이름 : 
+      <input type="text" value="" v-model="search">
+      </label>
+    </div>
+    
+    <label>Table</label>
+    <div>
+      <table class="wid100">
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>나이</th>
+            <th>연락처</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in filterItems" :key="item.name">
+            <td>{{ item['name'] }}</td>
+            <td>{{ item['old'] }}</td>
+            <td>{{ item['phone'] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div id="editor" :class="sectionDevide">
+      <h2>{{ msg }}</h2>
+    </div>
+  </div>
+  
+
+  <script>
+  // import { test } from "index.vue";
+    var vue = new Vue({
+      el: "#main",
+      data: {
+        msg: "hello, world !",
+        items: [
+          {
+            name: 'hy',
+            old: 2,
+            phone: "010-7521-9120"
+          }, 
+          {
+            name: 'chy',
+            old: 31,
+            phone: "010-1252-1535"
+          }
+        ],
+        search: '',
+        sectionDevide: "mt-20",
+      },
+      computed: {
+        filterItems: function() {
+            return this.items.filter(function(item) {
+              console.log(item);
+              console.log(item["name"].indexOf(this.search));
+              var res = item["name"].indexOf(this.search) < 0
+              return item["name"].indexOf(this.search) < 0;
+            })
+        }
+      }
+  });
+  
+  </script>
+  <script>
+    
+  </script>
+</body>
+</html>
---
+++ www/index.vue
@@ -0,0 +1,9 @@
+<script>
+const test = function() {
+    return "test 다 이 자식아 !";
+}
+
+export {
+    test 
+};
+</script>
